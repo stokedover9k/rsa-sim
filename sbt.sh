7968ef4f49b9626af1c72f5b7bfc50271a9f6b15
Yuriy Skobov <yuriy@yuriy-M17x.(none)>
2014-05-04 13:32:51 -0400

    [+][SETUP] README and SBT setup.

---
+++ sbt.sh
@@ -0,0 +1,7 @@
+if [ ! -f sbt-launch.jar ];
+then
+	wget http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.2/sbt-launch.jar
+fi
+
+SBT_OPTS="-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
+java $SBT_OPTS -jar `dirname $0`/sbt-launch.jar "$@"
